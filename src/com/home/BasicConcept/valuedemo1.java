1939d61c28bc90053dd4f5fdc8bd1b7b9bdcd5d3
Eascaty <510252916@qq.com>
2023-01-10 23:42:14 +0800

    字面量、变量、计算机中的数据存储

---
+++ src/com/home/BasicConcept/valuedemo1.java
@@ -0,0 +1,57 @@
+package com.home.BasicConcept;
+
+
+/**
+ * 字面量
+ */
+public class valuedemo1 {
+    //        关键字全部小写
+    public static void main(String[] args) {
+
+//        字符串类型带双引号
+//        "helloworld","黑马程序员"
+
+//    字符类型
+//      'A','B','C','D','E','F','我'
+//
+//      整数输出
+        System.out.println(666);
+        System.out.println(-777);
+
+//        小数
+        System.out.println(1.93);
+        System.out.println(-3.71);
+
+//        字符串
+        System.out.println("黑马程序员");
+        System.out.println("我是大帅哥");
+
+//    字符
+        System.out.println('男');
+        System.out.println('女');
+
+//        布尔
+        System.out.println(true);
+        System.out.println(false);
+
+//        空
+//        细节：null是不能直接打印的
+//        只能用字符串的方式打印出Null
+        System.out.println("null");
+
+
+//        '\t'制表符
+//        在打印的时候，把前面字符串的长度补齐到8或者是8的倍数。最少补一个空格，最多补8个空格
+        System.out.println("tom23"+'\t'+ "age");
+
+        int a = 10 ;
+        System.out.println(a);//10
+
+
+    }
+
+
+
+
+
+}
